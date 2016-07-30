3462494243256b3949346afa3489d66da636e65b
Josh Rabideaux <jrabideaux@Joshs-MBP.hsd1.mn.comcast.net>
2016-07-30 16:08:23 -0500

    Setup new maven project

---
+++ blackjack/src/main/java/com/chaosgames/blackjack/App.java
@@ -0,0 +1,13 @@
+package com.chaosgames.blackjack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
